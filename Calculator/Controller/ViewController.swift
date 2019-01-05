040acdba3b60b3e11c2946e3990aa9e15da8c152
Cavit KULAKLI <15360190+cavitkulakli@users.noreply.github.com>
2019-01-05 18:34:49 +0300

    % , +/- , AC button action added & multiple . bug fix

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -14,13 +14,34 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         
         isFinishedTypingNumber = true
-    
+        
+        if let calcMethod = sender.currentTitle {
+            
+            let calculator = CalculatorLogic(number: displayValue)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else {
+                fatalError("The result of the calculation is nil.")
+            }
+            displayValue = result
+        }
     }
 
     
@@ -35,6 +56,16 @@ class ViewController: UIViewController {
                 
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
